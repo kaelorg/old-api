fe49a70c2608446b11e972140d2583bfa1cb8ecb
Pablo Vinícius <dev.pablo.vinicius@gmail.com>
2020-07-14 17:11:14 -0300

    feat: project base

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+  extends: ['airbnb-base', 'eslint:recommended', 'plugin:prettier/recommended'],
+  plugins: ['import', 'prettier'],
+  env: {
+    node: true,
+    es6: true,
+    jest: true,
+  },
+  globals: {
+    use: 'readonly',
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaVersion: 2019,
+  },
+  rules: {
+    'prettier/prettier': 'error',
+    'global-require': 'off',
+    'consistent-return': 'off',
+    'class-methods-use-this': 'off',
+
+    'no-console': 'off',
+    'no-param-reassign': 'off',
+    'no-underscore-dangle': 'off',
+    'no-restricted-globals': 'off',
+  },
+};
