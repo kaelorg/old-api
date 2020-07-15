3f91b081bc55817926f38e1572a7793d62bd464c
Pablo Vinícius <dev.pablo.vinicius@gmail.com>
2020-07-15 16:54:22 -0300

    constants

---
+++ src/utils/constants.js
@@ -0,0 +1,19 @@
+module.exports = {
+  IMAGES: {
+    BACKGROUND: 'https://i.imgur.com/XVNmOUb.png',
+  },
+
+  DEFAULTS: {
+    PREFIX: '-',
+    LANGUAGE: 'pt-BR',
+    FAV_COLOR: '#eb798d',
+
+    LEVEL_UP_MESSAGE: 'Parabéns [user], você passou para o nível **[nivel]**!',
+    BIOGRAPHY:
+      'Oi, eu sou o Kael. Um porquinho charmoso que alegra seu dia. Para alterar a sua biografia acesse meu dashboard!',
+
+    LEAVE_WELCOME_MESSAGE: '[user] saiu do servidor!',
+    INPUT_WELCOME_MESSAGE: '[user] entrou no servidor!',
+    PRIVATE_WELCOME_MESSAGE: '[user] obrigado por entrar no servidor!',
+  },
+};
