60c23755e230192ac176ed0c62bb1cbe36eccc57
Pablo Vinícius <dev.pablo.vinicius@gmail.com>
2020-07-16 16:58:33 -0300

    chore: user controller worked

--- app/Controllers/Http/UserController.js
+++ app/Controllers/Http/UserController.js
@@ -1,6 +1,25 @@
+/** @type {import('../../Models/User')} */
+const User = use('App/Models/User');
+
 class UserController {
-  main() {
-    return [{ id: 1 }, { id: 2 }];
+  async user({ auth, params: { userId } }) {
+    const helpers = userId === '@me' ? auth.user : await User.findOne(userId);
+
+    return {
+      helpers,
+    };
+  }
+
+  async userGuilds({ discord }) {
+    const guilds = await discord.user.getGuilds();
+    return guilds;
+  }
+
+  async userGuild({ discord, response, params: { guildId } }) {
+    const { code, ...guild } = await discord.user.getGuild(guildId);
+
+    if (code) return response.status(code).send();
+    return guild;
   }
 }
 
