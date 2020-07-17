81af6106a8100659d1e94cc401965c68a6514f6a
Pablo Vinícius <dev.pablo.vinicius@gmail.com>
2020-07-17 15:49:41 -0300

    routes

--- start/routes.js
+++ start/routes.js
@@ -1,18 +1,26 @@
 /** @type {typeof import('@adonisjs/framework/src/Route/Manager')} */
 const Route = use('Route');
 
-// User Routes
-Route.group('Users', () => {
-  Route.get(':userId', 'UserController.user');
-  Route.get('@me/guilds', 'UserController.userGuilds');
-  Route.get('@me/guilds/:guildId', 'UserController.userGuild');
-})
-  .prefix('users')
-  .middleware(['auth', 'discord']);
-
 // Auth Routes
+
 Route.group('Auth', () => {
-  Route.get('redirect', 'AuthController.redirect');
-  Route.get('callback', 'AuthController.authenticate');
-  Route.get('verify', 'AuthController.verify').middleware('auth');
-}).prefix('auth');
+  Route.get('/redirect', 'AuthController.redirect');
+  Route.get('/callback', 'AuthController.authenticate');
+  Route.get('/verify', 'AuthController.verify').middleware('auth');
+}).prefix('/auth');
+
+// User Routes
+
+Route.group('Users', () => {
+  Route.get('/:userId', 'UserController.user');
+  Route.get('/@me/guilds', 'UserController.userGuilds');
+  Route.get('/@me/guilds/:guildId', 'UserController.userGuild');
+
+  // Edit user
+
+  Route.put('/@me/profile', 'UserController.editUserProfile').validator(
+    'UserProfile',
+  );
+})
+  .prefix('/users')
+  .middleware(['auth', 'discord']);
