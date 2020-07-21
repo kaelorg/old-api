77eca074fb1a86288ce3954327f034fae84232ee
Pablo Vinícius <dev.pablo.vinicius@gmail.com>
2020-07-21 18:47:12 -0300

    chore: Self-signed certificate

--- server.js
+++ server.js
@@ -1,7 +1,21 @@
+const pem = require('pem');
+const https = require('https');
 const { Ignitor } = require('@adonisjs/ignitor');
 
-new Ignitor(require('@adonisjs/fold'))
-  .appRoot(__dirname)
-  .wsServer()
-  .fireHttpServer()
-  .catch(console.error);
+pem.createCertificate({ days: 1, selfSigned: true }, (error, keys) => {
+  if (error) return console.error(error);
+
+  new Ignitor(require('@adonisjs/fold'))
+    .appRoot(__dirname)
+    .wsServer()
+    .fireHttpServer(handler => {
+      return https.createServer(
+        {
+          key: keys.serviceKey,
+          cert: keys.certificate,
+        },
+        handler,
+      );
+    })
+    .catch(console.error);
+});
