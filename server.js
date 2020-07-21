3d942a94af9c6152eb0bfac0305e814305de86f7
Pablo Vinícius <dev.pablo.vinicius@gmail.com>
2020-07-21 19:26:46 -0300

    fixes

--- server.js
+++ server.js
@@ -1,21 +1,7 @@
-const pem = require('pem');
-const https = require('https');
 const { Ignitor } = require('@adonisjs/ignitor');
 
-pem.createCertificate({ days: 1, selfSigned: true }, (error, keys) => {
-  if (error) return console.error(error);
-
-  new Ignitor(require('@adonisjs/fold'))
-    .appRoot(__dirname)
-    .wsServer()
-    .fireHttpServer(handler => {
-      return https.createServer(
-        {
-          key: keys.serviceKey,
-          cert: keys.certificate,
-        },
-        handler,
-      );
-    })
-    .catch(console.error);
-});
+new Ignitor(require('@adonisjs/fold'))
+  .appRoot(__dirname)
+  .wsServer()
+  .fireHttpServer()
+  .catch(console.error);
