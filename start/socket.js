7d040c28a4c974cb6494ccb393a845f1dead472b
Pablo Vinícius <dev.pablo.vinicius@gmail.com>
2020-11-04 02:46:56 -0300

    chore: add guild ws channels

--- start/socket.js
+++ start/socket.js
@@ -2,3 +2,6 @@
 const Ws = use('Ws');
 
 Ws.channel('vanity', 'GuildVanityController');
+Ws.channel('role-all', 'GuildRoleAllController');
+Ws.channel('ban-freeze', 'GuildBanFreezeController');
+Ws.channel('suggestion', 'GuildSuggestionController');
